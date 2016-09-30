58bd602f09c6044e5fcba4ca7aae5302bb9d4be0
Tiraj Parikh <Tiraj Parikh>
2016-09-29 19:28:25 -0500

    Adding tests, and fixing UI

---
+++ WordLadder/src/assignment3/TestGeneral.java
@@ -0,0 +1,71 @@
+package assignment3;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class TestGeneral {
+
+	@Test
+	public void testIfNotEmptyBFS() {
+
+		assertNotEquals(Collections.EMPTY_LIST, Main.getWordLadderBFS("SMART", "HONEY"));
+	}
+
+	@Test
+	public void testIfDifSizeBFS() {
+
+		assertEquals(Collections.EMPTY_LIST, Main.getWordLadderBFS("SMART", ""));
+	}
+
+	@Test
+	public void testIfSameWordBFS() {
+
+		assertEquals(1, Main.getWordLadderBFS("MONEY", "MONEY").size());
+	}
+
+	@Test
+	public void testIfEmptyBFS() {
+
+		assertEquals(Collections.EMPTY_LIST, Main.getWordLadderBFS("", "FUNNY"));
+	}
+
+	@Test
+	public void testIfFirstLetterDifBFS() {
+
+		assertEquals(2, Main.getWordLadderBFS("MONEY", "HONEY").size());
+	}
+
+	@Test
+	public void testIfNotEmptyDFS() {
+
+		assertNotEquals(Collections.EMPTY_LIST, Main.getWordLadderBFS("SMART", "HONEY"));
+	}
+
+	@Test
+	public void testIfDifSizeDFS() {
+
+		assertEquals(Collections.EMPTY_LIST, Main.getWordLadderDFS("SMART", "CENA"));
+	}
+
+	@Test
+	public void testIfSameWordDFS() {
+
+		assertEquals(1, Main.getWordLadderDFS("MONEY", "MONEY").size());
+	}
+
+	@Test
+	public void testIfEmptyDFS() {
+
+		assertEquals(Collections.EMPTY_LIST, Main.getWordLadderDFS("", "FUNNY"));
+	}
+
+	@Test
+	public void testIfFirstLetterDifDFS() {
+
+		assertEquals(2, Main.getWordLadderDFS("MONEY", "HONEY").size());
+	}
+
+}
